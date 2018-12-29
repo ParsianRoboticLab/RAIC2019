059671c0decc237a8e5a5f64028f266865f20520
Mohammad Mahdi Rahimi <mohammadmahdi76@Gmail.com>
2018-12-30 00:08:12 +0330

    add circle2d

--- src/circle2.rs
+++ src/circle2.rs
@@ -37,14 +37,14 @@ impl Circle2 {
     }
 
     fn intersection_line(&self, l : &Line2) -> (usize, Vec2, Vec2) {
-        let mut sol1 : Vec2;
-        let mut sol2 : Vec2;
+        let mut sol1 = VEC2INVALID;
+        let mut sol2 = VEC2INVALID;
         if l.a.abs() < std::f64::EPSILON {
             if l.b.abs() < std::f64::EPSILON {
                 return (0, VEC2INVALID, VEC2INVALID);
             }
-            let mut x1 : f64;
-            let mut x2 : f64;
+            let mut x1 = INVALID_;
+            let mut x2 = INVALID_;
             let n_sol = QUADRATIC_FOMULA(&1.0, &(-2.0 * self.center.x),
                             &((SQUARE(&self.center.x))
                             + (SQUARE(&(l.c / l.b + self.center.y)))
@@ -57,13 +57,23 @@ impl Circle2 {
                     sol2.assign(x2, y1);
                 }
             }
-            return (n_sol, sol1, sol2);
+            (n_sol, sol1, sol2)
         } else {
             let m = l.b / l.a;
             let d = l.c / l.a;
             let a = 1.0 * m * m;
             let b = 2.0 * (-self.center.y + (d + self.center.x) * m);
-            let c = SQUARE(d + self.center.x) + SQUARE(self.center.y) - SQUARE(self.radius);
+            let c = SQUARE(&(d + self.center.x)) + SQUARE(&self.center.y) - SQUARE(&self.radius);
+            let mut y1 : f64 = 0.0;
+            let mut y2 : f64 = 0.0;
+            let n_sol = QUADRATIC_FOMULA(&a, &b, &c, &mut y1, &mut y2);
+            if n_sol > 0 {
+                sol1.assign(l.getX(y1), y1);
+            }
+            if n_sol > 1 {
+                sol2.assign(l.getX(y2), y2);
+            }
+            (n_sol, sol1, sol2)
         }
 
     }
